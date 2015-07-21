34400d90e27f86340720711bb86591ce0b8f7c16
Philip Goddard <pmgoddard89@gmail.com>
2015-07-21 16:38:45 +0100

    string search and replace added

--- ggPanelPlot.R
+++ ggPanelPlot.R
@@ -2,7 +2,7 @@
 # often handy to have all predictors in panels for machine
 # learning problems!
 # Aim: to automatically cycle through predictors, and plot
-# ggplot density plots to examine distributions 
+# ggplot density plots to examine distributions
 
 library(ggplot2)
 library(mlbench)
@@ -17,7 +17,7 @@ j <- 1
 plotList <- list()
 for(i in colNames){
   plt <- ggplot(Glass, aes_string(x=i)) + geom_density() + geom_rug()
-  assign(paste("plot", j, sep = ""), plt)   
+  assign(paste("plot", j, sep = ""), plt)
   j <- j + 1
   plotList[[i]] <- plt
 }
@@ -29,7 +29,7 @@ for(i in colNames){
   Sys.sleep(2)
 }
 
-# but a multiplot would be far more handy! 
+# but a multiplot would be far more handy!
 # this modifies http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
 # reads in a list of plots (which may be tedious to do one by one) and plots!
 
@@ -37,12 +37,12 @@ multiplotList(plotList[1:9],cols=3)
 
 multiplotList <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   library(grid)
-  
+
   # Make a list from the ... arguments and plotlist
   plots <-  c(..., plotlist)
-  
+
   numPlots = length(plots)
-  
+
   # If layout is NULL, then use 'cols' to determine layout
   if (is.null(layout)) {
     # Make the panel
@@ -51,20 +51,20 @@ multiplotList <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
                      ncol = cols, nrow = ceiling(numPlots/cols))
   }
-  
+
   if (numPlots==1) {
     print(plots[[1]])
-    
+
   } else {
     # Set up the page
     grid.newpage()
     pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
+
     # Make each plot, in the correct location
     for (i in 1:numPlots) {
       # Get the i,j matrix positions of the regions that contain this subplot
       matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
+
       print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
                                       layout.pos.col = matchidx$col))
     }
---
+++ string_search_replace.R
@@ -0,0 +1,42 @@
+# function to take in a vector of search strings, vector of replacement strings
+# option to use regexp or direct subsetting (which may be faster,
+# although not as flexible)
+
+# test that works for lapply()? eg if have cahnges to make for multiple columns
+# implement foreach to do in parallel?
+
+replace <- function(x, curr, replace, regex = FALSE, ...) {
+  stopifnot(length(curr) == length(replace))
+  stopifnot(is.character(curr) && is.character(replace))
+  
+  for(i in 1:length(curr)){ 
+    if(regex){
+      x <- gsub(curr[i], replace[i], x, ...)
+    } else {
+      x[x == curr[i] ] <- replace[i]
+    }
+  }
+  x
+}
+
+test <- data.frame(x = c('b', 'b', 'h', 'k', 'k', '1', '+'),
+                   y = c(1, 2, 3, 4, 5, 6, 7))
+test$x <- as.character(test$x)
+
+abbrev <- c('b', 'k', '1', '+')
+desired <- c('1e9', '1e3', '0', '0')
+
+test$x
+replace(test$x, abbrev, desired)
+
+
+
+test2 <- data.frame(x = c('b', 'B', 'b', 'B', 'b', 'b', 'b'),
+                    y = c(1, 2, 3, 4, 5, 6, 7))
+
+test2$x <- as.character(test2$x)
+abbrev2 <- c('[bB]')
+desired2 <- c('1e9')
+
+test2$x
+replace(test2$x, abbrev2, desired2, regex = T)
